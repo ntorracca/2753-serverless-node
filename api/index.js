7d02e7aa6dfdf0990964ec0233e7e6f67ea07b1c
ntorracca <ntorracca@gmail.com>
2026-05-18 16:34:01 -0300

    feat: :sparkles:

--- api/index.js
+++ api/index.js
@@ -1,13 +1,68 @@
-module.exports.handler = async (event) => {
+'use strict'
+const { randomUUID } = require('crypto')
+const previousResults = new Map()
+
+function extractBody(event) {
+  if (!event?.body) {
+    return {
+      statusCode: 422,
+      body: JSON.stringify( {error: 'Missing body'} )
+    }
+  } 
+  return JSON.parse(event.body)
+}
+
+module.exports.sendResponse = async(event) => {
+  const { name, answers } = extractBody(event)
+  const correctQuestions = [3, 1, 0, 2]
+  const correctAnswers = answers.reduce((acc, answer, index) => {
+    if (answer === correctQuestions[index]) {
+      acc++
+    }
+    return acc
+  }, 0)
+
+  const result = {
+    name,
+    correctAnswers,
+    totalAnswers: answers.length
+  }
+
+  const resultId = randomUUID()
+  previousResults.set(resultId, { response: req.body, result })
+  
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+    statusCode: 201,
+    body: JSON.stringify( {
+      resultId,
+    __hypermedia: {
+      href: `/results.html`,
+      query: { id: resultId }
+    }
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  
+  module.exports.getResult = async (event) => {
+    const result = previousResults.get(event.pathParameters.id)
+    if (!result) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify( { error: 'Result not found' } ),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    }
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
       },
-      null,
-      2
-    ),
-  };
-};
+      body: JSON.stringify(result)
+    }
+  }
+  
+}
